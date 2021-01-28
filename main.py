9147035b33f585e3fa90a02ef03946c77e4b42f3
barbelloper <ssytn2@naver.com>
2021-01-28 07:36:10 +0000

    make the code shorter

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 def solution(s): 
-  a=sorted(s) 
-  for x in range(len(a)-1):
-    for y in range(x+1,len(a)):
-      if a[x]<a[y]:
-        temp = a[x]
-        a[x] = a[y]
-        a[y] = temp   
-
+  a=sorted(s,reverse=True) 
   answer = ''
   for x in range(len(a)):
     answer +=a[x]
