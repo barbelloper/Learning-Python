2108a5397e4230af7895087bc7e38bd0103a37ca
barbelloper <ssytn2@naver.com>
2021-01-29 09:02:41 +0000

    change 124world

--- lv2/124world.py
+++ lv2/124world.py
@@ -1,31 +1,13 @@
-def solution(n):
-  #124나라의 숫자 규칙 1 : 4진법 숫자에서 일정한 수를 더한 숫자이다.
-    cnt=0
-    for x in range(2,n): #2부터 n까지의 수를 3으로 나누었을 때 나머지가 1인 숫자의 개수만큼 +1씩 커진다.
-      if x%3 ==1:       #그 커진 수를 4진법으로 변환한 후 0은 1로 3은 4로 변환하면 124나라 숫자가 완성.
-        cnt+=1
-    n=n+cnt #매개변수로 받은 10진법 수를 4진법으로 고쳤을때 10진법으로 변환
-    arr=[]  #124나라의 각자리 숫자를 받을 리스트 
-    OTF_world=0 #124나라의 각자리 숫자
-    answer=0
-    while True:
-        OTF_world=n%4
-        if(n%4==3):
-            OTF_world=4                 
-        if(n%4==0):
-            OTF_world=1                  #4진법 변환
-        arr.append(OTF_world)
-        if((n//4)<1):
-          break
-        n=n//4
-       
-    arr_len=len(arr)
-    
-    for x in range(arr_len):       #리스트를 정수형으로 변환
-        answer+=arr[x]*10**x  
-    
-    answer=str(answer)    #정수를 문자열로 변환
+def change124(n):
+    num = ['1','2','4']
+    answer = ""
+    while n>0:
+      n-=1
+      answer=num[n%3]+answer
+      n//=3
+
     return answer
 
 
-print(solution(9825))
+# 아래는 테스트로 출력해 보기 위한 코드입니다.
+print(change124(2))
