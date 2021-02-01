bb243d4aa4aa73bb01bb70888cbbb116466c6280
barbelloper <ssytn2@naver.com>
2021-02-01 12:22:13 +0000

    str

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-def multpoly(A,B) :
-  answer = [[0 for _ in range(len(B[0]))] for _ in range(len(A))]
+def solution(s):
+    s_list=[]
+    for i in s:
+        s_list.append(i)
+    s_list[0]=s_list[0].upper()
+    for j in range(1,len(s_list)):    
+        if s_list[j].isupper()==True and s_list[j-1]==' ':
+            s_list[j]=s_list[j].lower()
+        elif s_list[j].islower()==True and s_list[j-1]==' ':
+            s_list[j]=s_list[j].upper()
+        else:
+            s_list[j]=s_list[j].lower()
 
-  for x in range(len(A)):
-    for y in range(len(B[0])):
-      for z in range(len(A[0])):
-         answer[x][y] += (A[x][z]*B[z][y])
-  
-  
-  return answer
+    for x in range(1,len(s_list)):
+        s_list[0] +=s_list[x]
 
-print(multpoly([[1, 4], [3, 2], [4, 1]],[[3,3],[3,3]]))
+    answer = s_list[0]
+    return answer
+
+print(solution('ADV deadf ADF advE'))
